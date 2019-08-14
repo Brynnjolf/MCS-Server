92986e3b946052e0c5354efda48ed9dee39d20ce
Brynn <therealmcs58@gmail.com>
2019-08-14 04:02:04 +0000

    Set up Django admin page for database interactivity

--- main/urls.py
+++ main/urls.py
@@ -4,8 +4,6 @@ from . import views
 
 app_name = 'main'
 urlpatterns = [
-    path('', views.index, name = 'index')
-    # path('scrape/', views.run_scraper, name = 'run_scraper'),
-    # path('search/', views.searchTable, name='searchTable'),
-    # path('<str:company_id>/', views.summary, name='summary')
+    path('', views.index, name = 'index'),
+    path('<str:company_id>/', views.summary, name='summary'),
 ]   
