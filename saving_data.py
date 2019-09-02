9b944690ed41ba99f2cbca9b855a3ec396f5ee8b
Unknown <mbk3801@autuni.ac.nz>
2019-09-02 23:29:07 +1200

    Saves Price and Dividend

--- saving_data.py
+++ saving_data.py
@@ -8,6 +8,7 @@ def save_json_data(payload):
             for companyKey, companyValue in scrapeData.items():
                 if companyKey != 'Date':
                     companyModel = Company(ticker= companyKey, name= companyValue['Summary']['Name'], sector= companyValue['Profile']['\nBusiness Description\n'])
+                    companyModel.save()
                     #print(companyKey, companyValue['Summary']['Name'], companyValue['Profile']['\nBusiness Description\n'])
                     for companyDataKey, companyDataValue in companyValue.items():
                         if companyDataKey == "Summary":
@@ -32,18 +33,27 @@ def save_json_data(payload):
                                 # print(profileKey, profileValue)
                                 pass
                         if companyDataKey == "HistoricalPrices":
+                            priceList = []
                             for date, priceData in companyDataValue.items():
                                 # print(date, priceData)
-                                pass
+                                priceList.append(
+                                    Price(
+                                        date = date,
+                                        ticker = companyModel,
+                                        price = priceData['Last'],
+                                        capital_adjusted = priceData['Capital Adjusted'],
+                                        volume_traded = priceData['Volume Traded'],
+                                        value_traded = priceData['Dollar Value Traded'],
+                                        number_of_trades = priceData['Trades'],
+                                        price_change = priceData['Change']))
+                                Price.objects.bulk_create(priceList, ignore_conflicts= True)
                         if companyDataKey == "HistoricalDividends":
                             divList = []
-                            # Entry.objects.bulk_create([
-                            # Entry(headline="Django 1.0 Released"),
-                            # Entry(headline="Django 1.1 Announced"),
-                            # Entry(headline="Breaking: Django is awesome")
-                            # ])
-
                             for date, dividend in companyDataValue.items():
                                 # print(date, dividend)
-                                divList.append(Dividends(date = date, ticker = companyModel, dividends = dividend))
-                            Dividends.objects.bulk_create(divList)
+                                divList.append(
+                                    Dividends(
+                                        date = date,
+                                        ticker = companyModel,
+                                        dividends = dividend))
+                            Dividends.objects.bulk_create(divList, ignore_conflicts= True)
--- webServer/settings.py
+++ webServer/settings.py
@@ -90,7 +90,7 @@ DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.postgresql_psycopg2",
         "NAME": config["DB_NAME"],
-        "USER": config["DB_NAME"],
+        "USER": config["DB_USER"],
         "PASSWORD": config["DB_PASS"],
         "HOST": "localhost",
         "PORT": "5432",
