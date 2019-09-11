c354396e072ef4d43f44f7a11de2fe1fe0f0e481
Brynnjolf <brynnengbers@gmail.com>
2019-09-11 15:35:58 +1200

    updated static content of filters page

--- static/filter/filter.js
+++ static/filter/filter.js
@@ -1,4 +1,16 @@
-$(document).ready(function() {
-    $(".selectpicker").selectpicker("refresh");
-    console.log('selectpicker reloaded');
+$('document').ready(function() {
+    // Click the generate button, will take all information in form and send it to API endpoint via AJAX
+    $('.generate').click(function() {
+        // TODO: send these three variables to an endpoint for processing
+        var risk = $('#risk').val();
+        var index = $('#index').val();
+        var blacklist = $('#blacklist').val();
+        console.log(risk,index,blacklist);
+        // pseudo loading for client immersion, using settimeout
+        setTimeout(function() {
+            // TODO: Change from redirecting to homepage, to redirecting to company table page
+            window.location = '/';
+        }, 3000);
+
+    });
 });
