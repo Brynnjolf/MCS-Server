d3d9ea8c89dbc7ea2dd3488090dfa365ec1f21cd
DrCoco <mbk3801@autuni.ac.nz>
2019-09-11 22:32:25 +1200

    Created Composite Primary Key, and implemented into all tables

--- main/models.py
+++ main/models.py
@@ -7,42 +7,55 @@ class Company(models.Model):
     name = models.CharField(max_length = 50)
     sector = models.CharField(max_length = 255)
 
+class Summary(models.Model):
+    ticker_date = ticker_date = models.CharField(max_length=13, primary_key=True)
+    date = models.DateTimeField('Date Scraped')
+    ticker = models.ForeignKey(Company, on_delete=models.CASCADE, db_column="ticker")
+    market_cap = models.BigIntegerField()
+    risk = models.DecimalField(decimal_places = 2, max_digits = 5)
+    debt_equity_index = models.DecimalField(decimal_places = 2, max_digits = 5)
+    net_dividend_yield_index = models.DecimalField(decimal_places = 2, max_digits = 5)
+    sharpe_ratio_index = models.DecimalField(decimal_places = 2, max_digits = 5)
+    return_on_equity_index = models.DecimalField(decimal_places = 2, max_digits = 5)
+    score = models.SmallIntegerField()
+
 class Directors(models.Model):
-    date = models.DateTimeField('Date Scraped', primary_key=True)
+    date = models.DateTimeField('Date Scraped')
+    name_date = models.CharField(max_length=99, primary_key=True)
     ticker = models.ForeignKey(Company, on_delete=models.CASCADE, db_column="ticker")
     director_name = models.CharField(max_length=100)
     director_role = models.CharField(max_length=100)
 
 class Dividends(models.Model):
-    date = models.DateTimeField('Date Scraped', primary_key=True)
+    ticker_date = models.CharField(max_length=13, primary_key=True)
+    date = models.DateTimeField('Date Scraped')
     ticker = models.ForeignKey(Company, on_delete=models.CASCADE, db_column="ticker")
     dividends = models.FloatField()
 
 class Ratios(models.Model):
-    date = models.DateTimeField('Date Scraped', primary_key=True)
+    ticker_date = models.CharField(max_length=13, primary_key=True)
+    date = models.DateTimeField('Date Scraped')
     ticker = models.ForeignKey(Company, on_delete=models.CASCADE, db_column="ticker")
-    market_cap = models.FloatField()
-    price_equity = models.FloatField()
-    EPS = models.FloatField()
-    NTA = models.FloatField()
-    DPS = models.FloatField()
-    beta_value = models.FloatField()
+    ePS = models.FloatField()
+    nTA = models.FloatField()
+    net_DPS = models.FloatField()
+    gross_DPS = models.FloatField()
+    beta_Value = models.FloatField()
     price_NTA = models.FloatField()
-    Net_yield = models.FloatField()
-    Sharpe = models.FloatField()
-    debt_equity = models.FloatField()
-    current = models.FloatField()
-    ROE = models.FloatField()
-    ROA = models.FloatField()
-    quick_ratio = models.FloatField()
+    net_Yield = models.FloatField()
+    gross_Yield = models.FloatField()
+    sharpe = models.FloatField()
+    debt_Equity = models.FloatField()
+    return_On_Equity = models.FloatField()
 
 class IncomeStatement(models.Model):
-    date = models.DateTimeField('Date Scraped', primary_key=True)
+    year = models.DateTimeField('Date Scraped')
+    ticker_year = models.CharField(max_length=7, primary_key=True)
     ticker = models.ForeignKey(Company, on_delete=models.CASCADE, db_column="ticker")
     sales_revenue = models.FloatField()
     investment_revenue = models.FloatField()
     total_core_revenue = models.FloatField()
-    EB17DA = models.FloatField()
+    EBITDA = models.FloatField()
     depreciation = models.FloatField()
     EBIT = models.FloatField()
     interest_expense = models.FloatField()
@@ -55,7 +68,8 @@ class IncomeStatement(models.Model):
     diluted_eps = models.FloatField()
 
 class Price(models.Model):
-    date = models.DateTimeField('Date Scraped', primary_key=True)
+    date = models.DateTimeField('Date Scraped')
+    ticker_date = models.CharField(max_length=13, primary_key=True)
     ticker = models.ForeignKey(Company, on_delete=models.CASCADE, db_column="ticker")
     price = models.FloatField()
     capital_adjusted = models.FloatField()
@@ -65,7 +79,8 @@ class Price(models.Model):
     price_change = models.FloatField()
 
 class CompanyProfile(models.Model):
-    date = models.DateTimeField('Date Scraped', primary_key=True)
+    date = models.DateTimeField('Date Scraped')
+    ticker_date = models.CharField(max_length=13, primary_key=True)
     ticker = models.ForeignKey(Company, on_delete=models.CASCADE, db_column="ticker")
     overview = models.TextField()
     performance = models.TextField()
@@ -73,15 +88,17 @@ class CompanyProfile(models.Model):
     description = models.TextField()
 
 class CashTable(models.Model):
-    date = models.DateTimeField('Date Scraped', primary_key=True)
+    year = models.DateTimeField('Date Scraped')
+    ticker_year = models.CharField(max_length=7, primary_key=True)
     ticker = models.ForeignKey(Company, on_delete=models.CASCADE, db_column="ticker")
     operating = models.FloatField()
     investing = models.FloatField()
     finance = models.FloatField()
     net_change = models.FloatField()
-    
+
 class Equity(models.Model):
-    date = models.DateTimeField('Date Scraped', primary_key=True)
+    year = models.DateTimeField('Date Scraped')
+    ticker_year = models.CharField(max_length=7, primary_key=True)
     ticker = models.ForeignKey(Company, on_delete=models.CASCADE, db_column="ticker")
     paid_in_capital = models.FloatField()
     reatained_earnings = models.FloatField()
@@ -90,7 +107,8 @@ class Equity(models.Model):
     total_equity = models.FloatField()
 
 class Liabilities(models.Model):
-    date = models.DateTimeField('Date Scraped', primary_key=True)
+    year = models.DateTimeField('Date Scraped')
+    ticker_year = models.CharField(max_length=7, primary_key=True)
     ticker = models.ForeignKey(Company, on_delete=models.CASCADE, db_column="ticker")
     payables = models.FloatField()
     current_debt = models.FloatField()
@@ -102,7 +120,8 @@ class Liabilities(models.Model):
     total_liability = models.FloatField()
 
 class Assets(models.Model):
-    date = models.DateTimeField('Date Scraped', primary_key=True)
+    year = models.DateTimeField('Date Scraped')
+    ticker_year = models.CharField(max_length=7, primary_key=True)
     ticker = models.ForeignKey(Company, on_delete=models.CASCADE, db_column="ticker")
     cash_equity = models.FloatField()
     NTR = models.FloatField()
--- saving_data.py
+++ saving_data.py
@@ -3,102 +3,120 @@ from datetime import datetime
 import json
 
 def save_json_data(payload):
-        payloadJSON = json.loads(payload)
-        for scrapeDate, scrapeData in payloadJSON.items():
-            for companyKey, companyValue in scrapeData.items():
-                if companyKey != 'Date':
-                    companyModel = Company(ticker= companyKey, name= companyValue['Summary']['Name'], sector= companyValue['Profile']['\nBusiness Description\n'])
-                    companyModel.save()
-                    #print(companyKey, companyValue['Summary']['Name'], companyValue['Profile']['\nBusiness Description\n'])
-                    for companyDataKey, companyDataValue in companyValue.items():
-                        if companyDataKey == "Summary":
-                            summaryList = []
-                            for summaryKey, summaryValue in companyDataValue.items():
-                                # print(summaryKey, summaryValue)
-                                pass
-                                
-                        if companyDataKey == "Directors":
-                            directorList = []
-                            for directorName, roleName in companyDataValue.items():
-                                # print(directorName, roleName)
-                                directorList.append(
-                                    Directors(
-                                        date = scrapeDate.replace('/','-'),
-                                        ticker = companyModel,
-                                        director_name = directorName,
-                                        director_role = roleName
-                                    )
-                                )
-                            Directors.objects.bulk_create(directorList, ignore_conflicts = True)
-                            print("Saved director information for ", companyKey)
-                        if companyDataKey == "Ratio":
-                            ratioList = []
-                            # ratioList.append(
-                            #     Ratios(
-                            #         class Ratios(models.Model):
-                            #         date = scrapeDate.replace('/','-'),
-                            #         ticker = companyModel
-                            #         market_cap = companyDataValue['\nOverview\n'],
-                            #         price_equity = companyDataValue['\nOverview\n'],
-                            #         EPS = companyDataValue['\nOverview\n'],
-                            #         NTA = companyDataValue['\nOverview\n'],
-                            #         DPS = companyDataValue['\nOverview\n'],
-                            #         beta_value = companyDataValue['\nOverview\n'],
-                            #         price_NTA = companyDataValue['\nOverview\n'],
-                            #         Net_yield = companyDataValue['\nOverview\n'],
-                            #         Sharpe = companyDataValue['\nOverview\n'],
-                            #         debt_equity = companyDataValue['\nOverview\n'],
-                            #         current = companyDataValue['\nOverview\n'],
-                            #         ROE = companyDataValue['\nOverview\n'],
-                            #         ROA = companyDataValue['\nOverview\n'],
-                            #         quick_ratio = companyDataValue['\nOverview\n'],
-                            #     )
-                            # )
-
-                        if companyDataKey == "FinancialProfile":
-                            # print(companyDataValue['Year'])
-                            for statementKey, statementValue in companyDataValue['Data'].items():
-                                # print(statementKey)
-                                pass
-                        if companyDataKey == "Profile":
-                            profileList = []
-                            # print(profileKey, profileValue)
-                            profileList.append(
-                                CompanyProfile(
+    payloadJSON = json.loads(payload)
+    for scrapeDate, scrapeData in payloadJSON.items():
+        for companyKey, companyValue in scrapeData.items():
+            if companyKey != 'Date':
+                companyModel = Company(ticker= companyKey, name= companyValue['Summary']['Name'], sector= companyValue['Profile']['\nBusiness Description\n'])
+                companyModel.save()
+                #print(companyKey, companyValue['Summary']['Name'], companyValue['Profile']['\nBusiness Description\n'])
+                for companyDataKey, companyDataValue in companyValue.items():
+                    if companyDataKey == "Summary":
+                        summaryList = []
+                        summaryList.append(
+                            Summary(
+                                ticker_date = companyKey + scrapeDate.replace('/','-'),
+                                ticker = companyModel,
+                                date = datetime.strftime(datetime.now(), "%Y-%m-%d") + " 00:00:00+1200",
+                                market_cap = companyDataValue['Market Cap'],
+                                risk = companyDataValue['Risk'],
+                                debt_equity_index = companyDataValue['Debt Equity Index'],
+                                net_dividend_yield_index = companyDataValue['Net Dividend Yield Index'],
+                                sharpe_ratio_index = companyDataValue['Sharpe Ratio Index'],
+                                return_on_equity_index = companyDataValue['Return on Equity Index'],
+                                score = companyDataValue['Score']*100,
+                            )
+                        )
+                        # print(summaryKey, summaryValue)
+                        Summary.objects.bulk_create(summaryList, ignore_conflicts = True)
+                        print("Saved summary information for ", companyKey)
+                    if companyDataKey == "Directors":
+                        directorList = []
+                        for directorName, roleName in companyDataValue.items():
+                            # print(directorName, roleName)
+                            directorList.append(
+                                Directors(
+                                    name_date = directorName + scrapeDate.replace('/','-'),
+                                    date = datetime.strftime(datetime.now(), "%Y-%m-%d %H:%M:%S") + "+1200",
                                     ticker = companyModel,
-                                    date = scrapeDate.replace('/','-'),
-                                    overview = companyDataValue['\nOverview\n'],
-                                    performance = companyDataValue['\nPerformance\n'],
-                                    outlook = companyDataValue['\nOutlook\n'],
-                                    description = companyDataValue['\nBusiness Description\n']
+                                    director_name = directorName,
+                                    director_role = roleName
                                 )
                             )
-                            CompanyProfile.objects.bulk_create(profileList, ignore_conflicts= True)
-                            print("Saved profile data for ", companyKey)
-                        if companyDataKey == "HistoricalPrices":
-                            priceList = []
-                            for date, priceData in companyDataValue.items():
-                                # print(date, priceData)
-                                priceList.append(
-                                    Price(
-                                        date = date,
-                                        ticker = companyModel,
-                                        price = priceData['Last'],
-                                        capital_adjusted = priceData['Capital Adjusted'],
-                                        volume_traded = priceData['Volume Traded'],
-                                        value_traded = priceData['Dollar Value Traded'],
-                                        number_of_trades = priceData['Trades'],
-                                        price_change = priceData['Change']))
-                            Price.objects.bulk_create(priceList, ignore_conflicts= True)
-                            print("Saved price data for ", companyKey)
-                        if companyDataKey == "HistoricalDividends":
-                            divList = []
-                            for date, dividend in companyDataValue.items():
-                                # print(date, dividend)
-                                divList.append(
-                                    Dividends(
-                                        date = date,
-                                        ticker = companyModel,
-                                        dividends = dividend))
-                            Dividends.objects.bulk_create(divList, ignore_conflicts= True)
-                            print("Saved dividend data for ", companyKey)
+                        Directors.objects.bulk_create(directorList, ignore_conflicts = True)
+                        print("Saved director information for ", companyKey)
+                    if companyDataKey == "Ratio":
+                        ratioList = []
+                        ratioList.append(
+                            Ratios(
+                                ticker_date = companyKey + scrapeDate.replace('/','-'),
+                                date = datetime.strftime(datetime.now(), "%Y-%m-%d %H:%M:%S") + "+1200",
+                                ticker = companyModel,
+                                ePS = companyDataValue['EPS'],
+                                nTA = companyDataValue['NTA'],
+                                net_DPS = companyDataValue['Net DPS'],
+                                gross_DPS = companyDataValue['Gross DPS'],
+                                beta_Value = companyDataValue['Beta Value'],
+                                price_NTA = companyDataValue['Price/NTA'],
+                                net_Yield = companyDataValue['Net Yield'],
+                                gross_Yield = companyDataValue['Gross Yield'],
+                                sharpe = companyDataValue['Sharpe Ratio'],
+                                debt_Equity = companyDataValue['Debt Equity'],
+                                return_On_Equity = companyDataValue['Return on Equity']
+                            )
+                        )
+                        Ratios.objects.bulk_create(ratioList, ignore_conflicts=True)
+                        print("Saved ratio data for ", companyKey)
+
+                    if companyDataKey == "FinancialProfile":
+                        # print(companyDataValue['Year'])
+                        for statementKey, statementValue in companyDataValue['Data'].items():
+                            
+                            # print(statementKey)
+                            pass
+                    if companyDataKey == "Profile":
+                        profileList = []
+                        # print(profileKey, profileValue)
+                        profileList.append(
+                            CompanyProfile(
+                                ticker_date = companyKey + scrapeDate.replace('/','-'),
+                                ticker = companyModel,
+                                    date = datetime.strftime(datetime.now(), "%Y-%m-%d %H:%M:%S") + "+1200",
+                                overview = companyDataValue['\nOverview\n'],
+                                performance = companyDataValue['\nPerformance\n'],
+                                outlook = companyDataValue['\nOutlook\n'],
+                                description = companyDataValue['\nBusiness Description\n']
+                            )
+                        )
+                        CompanyProfile.objects.bulk_create(profileList, ignore_conflicts= True)
+                        print("Saved profile data for ", companyKey)
+                    if companyDataKey == "HistoricalPrices":
+                        priceList = []
+                        for date, priceData in companyDataValue.items():
+                            # print(date, priceData)
+                            # print(datetime.strftime(datetime.now(), "%Y-%m-%d %H:%M:%S"))
+                            priceList.append(
+                                Price(
+                                    ticker_date = companyKey + date,
+                                    date = datetime.strftime(datetime.now(), "%Y-%m-%d %H:%M:%S") + "+1200",
+                                    ticker = companyModel,
+                                    price = priceData['Last'],
+                                    capital_adjusted = priceData['Capital Adjusted'],
+                                    volume_traded = priceData['Volume Traded'],
+                                    value_traded = priceData['Dollar Value Traded'],
+                                    number_of_trades = priceData['Trades'],
+                                    price_change = priceData['Change']))
+                        Price.objects.bulk_create(priceList, ignore_conflicts= True)
+                        print("Saved price data for ", companyKey)
+                    if companyDataKey == "HistoricalDividends":
+                        divList = []
+                        for date, dividend in companyDataValue.items():
+                            # print(date, dividend)
+                            divList.append(
+                                Dividends(
+                                    ticker_date = companyKey + date,
+                                    date = datetime.strftime(datetime.now(), "%Y-%m-%d %H:%M:%S") + "+1200",
+                                    ticker = companyModel,
+                                    dividends = dividend))
+                        Dividends.objects.bulk_create(divList, ignore_conflicts= True)
+                        print("Saved dividend data for ", companyKey)
